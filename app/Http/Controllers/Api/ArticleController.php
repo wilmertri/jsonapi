807aca05f0331cfca0dc78838169c88ccb788e4d
Wilmer Fabian Triana <fabiantriana1072@gmail.com>
2023-08-25 11:53:00 -0500

    Route is implemented to obtain individual Articles

---
+++ app/Http/Controllers/Api/ArticleController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ArticleResource;
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    public function show(Article $article): ArticleResource
+    {
+        return ArticleResource::make($article);
+    }
+}
---
+++ tests/Feature/Articles/ListArticlesTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature\Articles;
+
+use App\Models\Article;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ListArticlesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function can_fetch_a_single_article(): void
+    {
+        $this->withoutExceptionHandling();
+        $article = Article::factory()->create();
+
+        $response = $this->getJson(route('api.v1.articles.show', $article));
+
+        $response->assertExactJson([
+            'data' => [
+                'type' => 'articles',
+                'id' => (string) $article->getRouteKey(),
+                'attributes' => [
+                    'title' => $article->title,
+                    'slug' => $article->slug,
+                    'content' => $article->content
+                ],
+                'links' => [
+                    'self' => route('api.v1.articles.show', $article)
+                ]
+            ]
+        ]);
+    }
+}
